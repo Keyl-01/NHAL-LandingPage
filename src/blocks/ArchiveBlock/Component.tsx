b1e92f647efdeed8f3cb98659f2aac28328902ca
Nguyen Doan Cuong <ndcuong2001@gmail.com>
2026-09-20 19:25:40 +0700

    feat: add glow effect in archive block

--- src/blocks/ArchiveBlock/Component.tsx
+++ src/blocks/ArchiveBlock/Component.tsx
@@ -29,7 +29,7 @@ export const ArchiveBlock: React.FC<ArchiveBlockProp> = ({
   return (
     <motion.section
       id="nhal-archive-section"
-      className="flex w-full justify-center"
+      className="relative flex w-full justify-center overflow-x-clip"
       {...SECTION_ENTRANCE}
     >
       <div
@@ -94,6 +94,68 @@ export const ArchiveBlock: React.FC<ArchiveBlockProp> = ({
             )}
           </motion.div>
         </div>
+      </div>
+
+      <div
+        className={cn(
+          'hidden flex-row flex-none items-center gap-2.5 h-[701px] p-0 absolute -bottom-[1026px] -left-[116px] -right-[116px] overflow-visible',
+          'md:flex',
+        )}
+      >
+        {/* Orange glow */}
+        <div
+          className={cn(
+            'bg-[radial-gradient(50%_50%,#e3940040_0%,#e3940000_100%)] flex-none w-[500px] h-[500px] absolute overflow-hidden',
+            'md:top-0 md:-left-[58px]',
+            'lg:-top-[147px] lg:-left-[105px]',
+          )}
+        />
+        {/* Blue glow */}
+        <div
+          className={cn(
+            'bg-[radial-gradient(50%_50%,#0097fe40_0%,#e3940000_100%)] flex-none w-[500px] h-[500px] absolute overflow-hidden',
+            'md:-top-[197px] md:-left-[336px]',
+            'lg:-bottom-[250px] lg:-left-[308px] lg:top-auto',
+          )}
+        />
+        {/* Green glow */}
+        <div
+          className={cn(
+            'bg-[radial-gradient(50%_50%,#00ad0940_0%,#e3940000_100%)] flex-none w-[500px] h-[500px] absolute overflow-hidden',
+            'md:-top-[149px] md:-right-[220px]',
+            'lg:-top-[214px] lg:-right-[134px]',
+          )}
+        />
+        {/* Pink glow */}
+        <div
+          className={cn(
+            'bg-[radial-gradient(50%_50%,#f347ff40_0%,#e3940000_100%)] flex-none w-[500px] h-[500px] absolute overflow-hidden',
+            'md:-bottom-[69px] md:-right-[285px]',
+            'lg:-bottom-[85px] lg:-right-[188px]',
+          )}
+        />
+        {/* Purple glow */}
+        <div
+          className={cn(
+            'bg-[radial-gradient(50%_50%,#9966ff40_0%,#e3940000_100%)] flex-none w-[500px] h-[500px] absolute -right-[220px] overflow-hidden',
+          )}
+        />
+        {/* Red glow */}
+        <div
+          className={cn(
+            'bg-[radial-gradient(50%_50%,#ff474740_0%,#e3940000_100%)] flex-none w-[500px] h-[500px] absolute overflow-hidden',
+            'md:-bottom-[150px] md:-left-[176px]',
+            'lg:-bottom-[90px] lg:-left-[191px]',
+          )}
+        />
+        {/* Blue-purple glow */}
+        <div
+          className={cn(
+            'bg-[radial-gradient(50%_50%,#4772ff40_0%,#e3940000_100%)] flex-none w-[500px] h-[500px] absolute overflow-hidden',
+            'md:top-[calc(50.0713%-250px)] md:-left-[97px]',
+            'lg:-bottom-[405px] lg:-left-[177px] lg:top-auto',
+          )}
+        />
       </div>
     </motion.section>
   )
